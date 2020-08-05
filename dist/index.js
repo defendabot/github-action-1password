3925ddf9b38b29607babe07b5b7f7dbd2c927720
Vlad Volkov <vlad@hiberbee.com>
2020-08-05 06:46:13 +0300

    Changed op device id

--- dist/index.js
+++ dist/index.js
@@ -1106,26 +1106,26 @@ const io_util_1 = __webpack_require__(672);
 const child_process_1 = __webpack_require__(129);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
+        const onePasswordVersion = core_1.getInput('version');
+        const platform = os_1.default.platform().toLowerCase();
+        const onePasswordUrl = `https://cache.agilebits.com/dist/1P/op/pkg/v${onePasswordVersion}/op_${platform}_amd64_v${onePasswordVersion}.zip`;
+        const destination = `${process.env.HOME}/bin`;
         try {
             const path = yield tool_cache_1.downloadTool(onePasswordUrl);
             const extracted = yield tool_cache_1.extractZip(path);
             yield io_1.mv(`${extracted}/op`, `${destination}/op`);
             yield io_util_1.chmod(`${destination}/op`, '0755');
             core_1.addPath(destination);
-            const output = child_process_1.execSync(`printf '%s' "${core_1.getInput('password')}" | op signin ${core_1.getInput('url')} ${core_1.getInput('email')} ${core_1.getInput('secret')} --raw`);
-            core_1.exportVariable('OP_SESSION_my', output.toString());
+            const deviceId = child_process_1.execSync('head -c 16 /dev/urandom | base32 | tr -d = | tr [:upper:] [:lower:]').toString();
+            const sessionToken = child_process_1.execSync(`printf '%s' "${core_1.getInput('password')}" | OP_DEVICE=${deviceId} op signin ${core_1.getInput('url')} ${core_1.getInput('email')} ${core_1.getInput('secret')} --raw`).toString();
+            core_1.exportVariable('OP_DEVICE', deviceId);
+            core_1.exportVariable('OP_SESSION_my', sessionToken);
         }
         catch (error) {
             core_1.setFailed(error.message);
         }
     });
 }
-const onePasswordVersion = core_1.getInput('version');
-const platform = os_1.default.platform().toLowerCase();
-const onePasswordUrl = `https://cache.agilebits.com/dist/1P/op/pkg/v${onePasswordVersion}/op_${platform}_amd64_v${onePasswordVersion}.zip`;
-const destination = `${process.env.HOME}/bin`;
-const deviceId = child_process_1.execSync('head -c 16 /dev/urandom | base32 | tr -d = | tr [:upper:] [:lower:]').toString();
-core_1.exportVariable('OP_DEVICE', deviceId);
 run();
 //# sourceMappingURL=main.js.map
 
--- src/main.ts
+++ src/main.ts
@@ -6,26 +6,27 @@ import { chmod } from '@actions/io/lib/io-util'
 import { execSync } from 'child_process'
 
 async function run(): Promise<void> {
+  const onePasswordVersion = getInput('version')
+  const platform = os.platform().toLowerCase()
+  const onePasswordUrl = `https://cache.agilebits.com/dist/1P/op/pkg/v${onePasswordVersion}/op_${platform}_amd64_v${onePasswordVersion}.zip`
+  const destination = `${process.env.HOME}/bin`
   try {
     const path = await downloadTool(onePasswordUrl)
     const extracted = await extractZip(path)
     await mv(`${extracted}/op`, `${destination}/op`)
     await chmod(`${destination}/op`, '0755')
     addPath(destination)
-    const output = execSync(
-      `printf '%s' "${getInput('password')}" | op signin ${getInput('url')} ${getInput('email')} ${getInput('secret')} --raw`,
-    )
-    exportVariable('OP_SESSION_my', output.toString())
+    const deviceId = execSync('head -c 16 /dev/urandom | base32 | tr -d = | tr [:upper:] [:lower:]').toString()
+    const sessionToken = execSync(
+      `printf '%s' "${getInput('password')}" | OP_DEVICE=${deviceId} op signin ${getInput('url')} ${getInput('email')} ${getInput(
+        'secret',
+      )} --raw`,
+    ).toString()
+    exportVariable('OP_DEVICE', deviceId)
+    exportVariable('OP_SESSION_my', sessionToken)
   } catch (error) {
     setFailed(error.message)
   }
 }
-
-const onePasswordVersion = getInput('version')
-const platform = os.platform().toLowerCase()
-const onePasswordUrl = `https://cache.agilebits.com/dist/1P/op/pkg/v${onePasswordVersion}/op_${platform}_amd64_v${onePasswordVersion}.zip`
-const destination = `${process.env.HOME}/bin`
-const deviceId = execSync('head -c 16 /dev/urandom | base32 | tr -d = | tr [:upper:] [:lower:]').toString()
-exportVariable('OP_DEVICE', deviceId)
 
 run()
